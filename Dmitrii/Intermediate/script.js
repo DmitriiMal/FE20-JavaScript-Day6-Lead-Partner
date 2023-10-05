b8d4ea9822e5b918c96fc2580db2b491a9d4d1fd
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-05 11:22:58 +0200

    complete Intermediate

--- Dmitrii/Intermediate/script.js
+++ Dmitrii/Intermediate/script.js
@@ -2,75 +2,105 @@
 // /// Intermediate Ex 1 /// //
 // ///////////////////////// //
 
-let employees = `
- [{
-     "userId": "EMP001",
-     "firstName": "John",
-     "lastName": "Smith",
-     "emailAddress": " john.smith@example.com"
-     "jobTitleName" : "Software Engineer",
-     "salary" : "80,000",
- }, {
-     "userId": "EMP002",
-     "firstName": "Emily",
-     "lastName": "Johnson",
-     "emailAddress": "emily.johnson@example.com"
-     "jobTitleName" : "Marketing Specialist",
-     "salary" : "65,000",
- }, {
-     "userId": "EMP003",
-     "firstName": "Michael",
-     "lastName": "Davis",
-     "emailAddress": "michael.davis@example.com"
-     "jobTitleName" : "Financial Analyst",
-     "salary" : "75,000",
- },{
-  "userId": "EMP004",
-  "firstName": "Sarah",
-  "lastName": "Lee",
-  "emailAddress": "sarah.lee@example.com"
-  "jobTitleName" : "Human Resources Manager",
-  "salary" : "70,000",
-}, {
-  "userId": "EMP005",
-  "firstName": "David",
-  "lastName": "Wilson",
-  "emailAddress": "david.wilson@example.com"
-  "jobTitleName" : "Graphic Designer",
-  "salary" : "60,000",
-}, {
-  "userId": "EMP006",
-  "firstName": "Jessica",
-  "lastName": "Brown",
-  "emailAddress": "jessica.brown@example.com"
-  "jobTitleName" : "Sales Representative",
-  "salary" : "55,000",
-},{
-  "userId": "EMP007",
-  "firstName": "Brian",
-  "lastName": "Anderson",
-  "emailAddress": "brian.anderson@example.com"
-  "jobTitleName" : "Customer Support Specialist",
-  "salary" : "50,000",
-}, {
-  "userId": "EMP008",
-  "firstName": "Olivia",
-  "lastName": "Martinez",
-  "emailAddress": "olivia.martinez@example.com"
-  "jobTitleName" : "Administrative Assistant",
-  "salary" : "45,000",
-}, {
-  "userId": "EMP009",
-  "firstName": "Daniel",
-  "lastName": "Thomas",
-  "emailAddress": "daniel.thomas@example.com"
-  "jobTitleName" : "IT Support Technician",
-  "salary" : "55,000",
-}, {
-  "userId": "EMP010",
-  "firstName": "Amanda",
-  "lastName": "White",
-  "emailAddress": "amanda.white@example.com"
-  "jobTitleName" : "Project Manager",
-  "salary" : "85,000",
-}]`;
+let employees = [
+  {
+    userId: 'EMP001',
+    firstName: 'John',
+    lastName: 'Smith',
+    emailAddress: ' john.smith@example.com',
+    jobTitleName: 'Software Engineer',
+    salary: '80,000',
+  },
+  {
+    userId: 'EMP002',
+    firstName: 'Emily',
+    lastName: 'Johnson',
+    emailAddress: 'emily.johnson@example.com',
+    jobTitleName: 'Marketing Specialist',
+    salary: '65,000',
+  },
+  {
+    userId: 'EMP003',
+    firstName: 'Michael',
+    lastName: 'Davis',
+    emailAddress: 'michael.davis@example.com',
+    jobTitleName: 'Financial Analyst',
+    salary: '75,000',
+  },
+  {
+    userId: 'EMP004',
+    firstName: 'Sarah',
+    lastName: 'Lee',
+    emailAddress: 'sarah.lee@example.com',
+    jobTitleName: 'Human Resources Manager',
+    salary: '70,000',
+  },
+  {
+    userId: 'EMP005',
+    firstName: 'David',
+    lastName: 'Wilson',
+    emailAddress: 'david.wilson@example.com',
+    jobTitleName: 'Graphic Designer',
+    salary: '60,000',
+  },
+  {
+    userId: 'EMP006',
+    firstName: 'Jessica',
+    lastName: 'Brown',
+    emailAddress: 'jessica.brown@example.com',
+    jobTitleName: 'Sales Representative',
+    salary: '55,000',
+  },
+  {
+    userId: 'EMP007',
+    firstName: 'Brian',
+    lastName: 'Anderson',
+    emailAddress: 'brian.anderson@example.com',
+    jobTitleName: 'Customer Support Specialist',
+    salary: '50,000',
+  },
+  {
+    userId: 'EMP008',
+    firstName: 'Olivia',
+    lastName: 'Martinez',
+    emailAddress: 'olivia.martinez@example.com',
+    jobTitleName: 'Administrative Assistant',
+    salary: '45,000',
+  },
+  {
+    userId: 'EMP009',
+    firstName: 'Daniel',
+    lastName: 'Thomas',
+    emailAddress: 'daniel.thomas@example.com',
+    jobTitleName: 'IT Support Technician',
+    salary: '55,000',
+  },
+  {
+    userId: 'EMP010',
+    firstName: 'Amanda',
+    lastName: 'White',
+    emailAddress: 'amanda.white@example.com',
+    jobTitleName: 'Project Manager',
+    salary: '85,000',
+  },
+];
+
+let employeesStr = JSON.stringify(employees);
+let employeesObj = JSON.parse(employeesStr);
+
+const result = document.querySelector('#result');
+
+console.table(employeesObj);
+
+for (let i = 0; i < employees.length; i++) {
+  result.innerHTML += `
+  <tr>
+  <th scope="row">${employeesObj[i].userId}</th>
+  <td>${employeesObj[i].firstName}</td>
+  <td>${employeesObj[i].lastName}</td>
+  <td>@${employeesObj[i].emailAddress}</td>
+  <td>@${employeesObj[i].jobTitleName}</td>
+  <td>@${employeesObj[i].salary}</td>
+  </tr>
+  `;
+}
